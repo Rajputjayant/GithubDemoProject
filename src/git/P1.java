90ecbe93f5f4ff2552f1dce54846e993a7ddfb5e
Jayant <Singh.jayant.jayant84@gmail.com>
2023-12-17 12:06:33 +0530

    First Commit....

---
+++ src/git/P1.java
@@ -0,0 +1,12 @@
+package git;
+
+public class P1 {
+
+	public static void main(String[] args) {
+         
+		System.out.println("Mango");
+		System.out.println("Apple");
+		System.out.println("Grapes");
+	}
+
+}
